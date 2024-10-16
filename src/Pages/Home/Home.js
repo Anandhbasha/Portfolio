bfd74965604fdbffce0412087732fc181ab8ed44
Anandhbasha <anadhakm@gmail.com>
2024-10-16 13:53:08 +0530

    Today_EOD

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './Home.css'; // Make sure to import the CSS file
+import './Home.css'; // Make sure to import your CSS file
 
 const Home = () => {
   return (
-    <div className="container">
-      <div className="gradient-overlay"></div>
-      <h1>Welcome to My Portfolio</h1>
-      <p>
-        As a passionate Freelance Instructor, I empower students and professionals through engaging training in Full Stack Development, Data Structures, and emerging technologies across diverse educational settings, including universities, corporate training programs, and hands-on workshops. Additionally, I am a skilled Website Developer with expertise in various frameworks and tools such as HTML, CSS, Bootstrap, JavaScript, jQuery, and PHP, focusing on creating dynamic and responsive websites that align with user needs and business objectives. Moreover, I possess a strong background in Lead Generation within the information technology and services industry, leveraging my skills in market research, adaptability, and problem-solving to drive growth and deliver value in a competitive landscape.
-      </p>
+    <div className='home'>
+      <div className="container">
+        <h1 className="fade-in">Welcome to My Portfolio</h1>
+        <p>
+          As a passionate Freelance Instructor, I empower students and professionals through engaging training in Full Stack Development, Data Structures, and emerging technologies across diverse educational settings, including universities, corporate training programs, and hands-on workshops. Additionally, I am a skilled Website Developer with expertise in various frameworks and tools such as HTML, CSS, Bootstrap, JavaScript, jQuery, and PHP, focusing on creating dynamic and responsive websites that align with user needs and business objectives. Moreover, I possess a strong background in Lead Generation within the information technology and services industry, leveraging my skills in market research, adaptability, and problem-solving to drive growth and deliver value in a competitive landscape.
+        </p>
+      </div>
     </div>
   );
 };
